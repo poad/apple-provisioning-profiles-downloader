295b853161a66a2d411e9acc69e45210fdca627c
Kenji Saito <ken-yo@mbr.nifty.com>
2021-09-18 01:06:44 +0900

    SWC build support

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,10 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig-test.json',
-      // useESM: true,
-    }
-  },
-  // extensionsToTreatAsEsm: ['.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
   moduleNameMapper: {
   },
