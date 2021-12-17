1f7820fb6ad58b717e7f48132c94ff5c10465e4a
Kenji Saito <ken-yo@mbr.nifty.com>
2021-12-18 08:39:47 +0900

    Support SWC

--- bin/main.js
+++ bin/main.js
@@ -7,124 +7,88 @@ import Client from './client.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export class ProvisioningProfileDownloader {
-    run = async () => {
-        try {
-            const bundleId = core.getInput('bundle-id', {
-                required: true,
-                trimWhitespace: true
-            });
-            const apiKeyId = core.getInput('api-key-id', {
-                required: true,
-                trimWhitespace: true
-            });
-            const issuerId = core.getInput('issuer-id', {
-                required: true,
-                trimWhitespace: true
-            });
-            const profileType = core.getInput('profile-type', {
-                required: false,
-                trimWhitespace: true
-            });
-            const apiPrivateKey = core.getInput('api-private-key', {
-                required: false,
-                trimWhitespace: true
-            });
-            const apiPrivateKeyFile = core.getInput('api-private-key-file', {
-                required: false,
-                trimWhitespace: true
-            });
-            const tokenDuration = core.getInput('token-duration', {
-                required: false,
-                trimWhitespace: true
-            });
-            if (apiPrivateKey.length === 0) {
-                const keyPath = path.resolve(__dirname, apiPrivateKeyFile);
-                const apiPrivateKeyFileCheck = fs.statSync(keyPath).isFile() || false;
-                if (!apiPrivateKeyFileCheck) {
-                    throw new Error("Specify either 'api-private-key' or 'api-private-key-file'.");
+    constructor(){
+        this.run = async ()=>{
+            try {
+                const bundleId = core.getInput('bundle-id', JSON.parse("{\"required\":true,\"trimWhitespace\":true}"));
+                const apiKeyId = core.getInput('api-key-id', JSON.parse("{\"required\":true,\"trimWhitespace\":true}"));
+                const issuerId = core.getInput('issuer-id', JSON.parse("{\"required\":true,\"trimWhitespace\":true}"));
+                const profileType = core.getInput('profile-type', JSON.parse("{\"required\":false,\"trimWhitespace\":true}"));
+                const apiPrivateKey = core.getInput('api-private-key', JSON.parse("{\"required\":false,\"trimWhitespace\":true}"));
+                const apiPrivateKeyFile = core.getInput('api-private-key-file', JSON.parse("{\"required\":false,\"trimWhitespace\":true}"));
+                const tokenDuration = core.getInput('token-duration', JSON.parse("{\"required\":false,\"trimWhitespace\":true}"));
+                if (apiPrivateKey.length === 0) {
+                    const keyPath = path.resolve(__dirname, apiPrivateKeyFile);
+                    const apiPrivateKeyFileCheck = fs.statSync(keyPath).isFile() || false;
+                    if (!apiPrivateKeyFileCheck) throw new Error("Specify either 'api-private-key' or 'api-private-key-file'.");
+                }
+                if (tokenDuration !== '' && Number.isNaN(tokenDuration)) throw new Error("The 'token-duration' must be an integer value.");
+                const duration = tokenDuration !== '' ? Number(tokenDuration) : undefined;
+                if (duration !== undefined && (duration < 1 || duration > 1200)) throw new Error("The 'token-duration' must be in the range of 1 to 1200.");
+                if (!process.env.HOME) throw new Error('Environment variable `HOME` is not defined!');
+                core.info(`bundle-id: ${bundleId}`);
+                core.info(`api-key-id: ${apiKeyId}`);
+                core.info(`issuer-id: ${issuerId}`);
+                core.info(`profile-type: ${profileType}`);
+                core.info(`api-private-key: ${apiPrivateKey ? 'specified' : undefined}`);
+                core.info(`api-private-key-file: ${apiPrivateKeyFile}`);
+                core.info(`token-duration: ${tokenDuration}`);
+                const privateKey = apiPrivateKey || fs.readFileSync(path.resolve(apiPrivateKeyFile));
+                const client = new Client({
+                    privateKey,
+                    issuerId,
+                    apiKeyId,
+                    duration
+                });
+                const response = await client.listBundleIds({
+                    'filter[identifier]': bundleId,
+                    include: 'profiles',
+                    'fields[profiles]': 'bundleId,certificates,createdDate,devices,expirationDate,name,platform,profileContent,profileState,profileType,uuid'
+                });
+                const bundleIds = response;
+                const profileIds = bundleIds.data.filter((value)=>value.attributes.identifier === bundleId && value.relationships.profiles !== undefined
+                ).flatMap((bundle)=>bundle.relationships.profiles.data
+                ).map((data)=>data.id
+                );
+                const profiles = bundleIds.included.filter((include)=>include.type === 'profiles' && profileIds.includes(include.id) && include.attributes.profileState === 'ACTIVE' && include.attributes.profileType === profileType
+                ).map((include)=>include
+                );
+                if (profiles.findIndex((profile)=>profile.attributes.uuid !== undefined && profile.attributes.profileContent
+                )) throw new Error('Profile attributes `uuid` and `profileContent` must be defined!');
+                const basePath = path.join(process.env.HOME, '/Library/MobileDevice/Provisioning Profiles');
+                await io.mkdirP(basePath);
+                profiles.map(async (profile)=>{
+                    const profileFilename = `${profile.attributes.uuid}.mobileprovision`;
+                    return {
+                        fullPath: path.join(basePath, profileFilename),
+                        profileType: profile.attributes.profileType,
+                        name: profile.attributes.name,
+                        content: profile.attributes.profileContent
+                    };
+                }).forEach(async (output)=>{
+                    const buffer = Buffer.from((await output).content, 'base64');
+                    fs.writeFileSync((await output).fullPath, buffer);
+                    core.info(`Wrote ${(await output).profileType} profile '${(await output).name}' to '${(await output).fullPath}'.`);
+                });
+                core.setOutput('profiles', JSON.stringify(profiles.map((value)=>{
+                    return {
+                        name: value.attributes.name,
+                        udid: value.attributes.uuid,
+                        type: value.attributes.profileType.toString()
+                    };
+                })));
+            } catch (error) {
+                if (error instanceof Error) {
+                    core.error(error);
+                    core.setFailed(error.message);
+                } else {
+                    core.error(JSON.stringify(error));
+                    core.setFailed(JSON.stringify(error));
                 }
             }
-            if (tokenDuration !== '' && Number.isNaN(tokenDuration)) {
-                throw new Error("The 'token-duration' must be an integer value.");
-            }
-            const duration = tokenDuration !== '' ? Number(tokenDuration) : undefined;
-            if (duration !== undefined && (duration < 1 || duration > 1200)) {
-                throw new Error("The 'token-duration' must be in the range of 1 to 1200.");
-            }
-            if (!process.env.HOME) {
-                throw new Error('Environment variable `HOME` is not defined!');
-            }
-            core.info(`bundle-id: ${bundleId}`);
-            core.info(`api-key-id: ${apiKeyId}`);
-            core.info(`issuer-id: ${issuerId}`);
-            core.info(`profile-type: ${profileType}`);
-            core.info(`api-private-key: ${apiPrivateKey ? 'specified' : undefined}`);
-            core.info(`api-private-key-file: ${apiPrivateKeyFile}`);
-            core.info(`token-duration: ${tokenDuration}`);
-            const privateKey = apiPrivateKey || fs.readFileSync(path.resolve(apiPrivateKeyFile));
-            const client = new Client({
-                privateKey,
-                issuerId,
-                apiKeyId,
-                duration
-            });
-            const response = await client.listBundleIds({
-                'filter[identifier]': bundleId,
-                include: 'profiles',
-                'fields[profiles]': 'bundleId,certificates,createdDate,devices,expirationDate,name,platform,profileContent,profileState,profileType,uuid'
-            });
-            const bundleIds = response;
-            const profileIds = bundleIds.data
-                .filter(value => value.attributes.identifier === bundleId &&
-                value.relationships.profiles !== undefined)
-                .flatMap(bundle => bundle.relationships.profiles.data)
-                .map(data => data.id);
-            const profiles = bundleIds.included
-                .filter(include => include.type === 'profiles' &&
-                profileIds.includes(include.id) &&
-                include.attributes.profileState === 'ACTIVE' &&
-                include.attributes.profileType === profileType)
-                .map(include => include);
-            if (profiles.findIndex(profile => profile.attributes.uuid !== undefined &&
-                profile.attributes.profileContent)) {
-                throw new Error('Profile attributes `uuid` and `profileContent` must be defined!');
-            }
-            const basePath = path.join(process.env.HOME, '/Library/MobileDevice/Provisioning Profiles');
-            await io.mkdirP(basePath);
-            profiles
-                .map(async (profile) => {
-                const profileFilename = `${profile.attributes.uuid}.mobileprovision`;
-                return {
-                    fullPath: path.join(basePath, profileFilename),
-                    profileType: profile.attributes.profileType,
-                    name: profile.attributes.name,
-                    content: profile.attributes.profileContent
-                };
-            })
-                .forEach(async (output) => {
-                const buffer = Buffer.from((await output).content, 'base64');
-                fs.writeFileSync((await output).fullPath, buffer);
-                core.info(`Wrote ${(await output).profileType} profile '${(await output).name}' to '${(await output).fullPath}'.`);
-            });
-            core.setOutput('profiles', JSON.stringify(profiles.map(value => {
-                return {
-                    name: value.attributes.name,
-                    udid: value.attributes.uuid,
-                    type: value.attributes.profileType.toString()
-                };
-            })));
-        }
-        catch (error) {
-            if (error instanceof Error) {
-                core.error(error);
-                core.setFailed(error.message);
-            }
-            else {
-                core.error(JSON.stringify(error));
-                core.setFailed(JSON.stringify(error));
-            }
-        }
-    };
+        };
+    }
 }
 new ProvisioningProfileDownloader().run();
+
 //# sourceMappingURL=main.js.map
