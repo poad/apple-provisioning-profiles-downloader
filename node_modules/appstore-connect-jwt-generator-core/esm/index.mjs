f7dc740ff47469fcd258ed17b4af5ece7ff26c59
Kenji Saito <ken-yo@mbr.nifty.com>
2022-05-09 08:15:42 +0900

    Bumps node modules

--- node_modules/appstore-connect-jwt-generator-core/esm/index.mjs
+++ node_modules/appstore-connect-jwt-generator-core/esm/index.mjs
@@ -1,19 +1,22 @@
 import jwt from 'jsonwebtoken';
-const signOption = (issuerId, privateKeyId, duration) => ({
-    algorithm: 'ES256',
-    keyid: privateKeyId,
-    audience: 'appstoreconnect-v1',
-    expiresIn: duration,
-    issuer: issuerId,
-});
-function jwtGenCore() { }
-export function tokenSync(privateKey, issuerId, privateKeyId, duration = 500) { return jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)); }
+const signOption = (issuerId, privateKeyId, duration)=>({
+        algorithm: 'ES256',
+        keyid: privateKeyId,
+        audience: 'appstoreconnect-v1',
+        expiresIn: duration,
+        issuer: issuerId
+    })
 ;
+function jwtGenCore() {}
+export function tokenSync(privateKey, issuerId, privateKeyId, duration = 500) {
+    return jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration));
+}
 export async function token(privateKey, issuerId, privateKeyId, duration = 500) {
     return Promise.resolve(jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)));
 }
-;
 jwtGenCore.tokenSync = tokenSync;
 jwtGenCore.token = token;
 export default jwtGenCore;
+
+
 //# sourceMappingURL=index.mjs.map
--- node_modules/appstore-connect-jwt-generator-core/lib/index.cjs
+++ node_modules/appstore-connect-jwt-generator-core/lib/index.cjs
@@ -1,28 +1,36 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.token = exports.tokenSync = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const signOption = (issuerId, privateKeyId, duration) => ({
-    algorithm: 'ES256',
-    keyid: privateKeyId,
-    audience: 'appstoreconnect-v1',
-    expiresIn: duration,
-    issuer: issuerId,
+Object.defineProperty(exports, "__esModule", {
+    value: true
 });
-function jwtGenCore() { }
-function tokenSync(privateKey, issuerId, privateKeyId, duration = 500) { return jsonwebtoken_1.default.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)); }
 exports.tokenSync = tokenSync;
-;
-async function token(privateKey, issuerId, privateKeyId, duration = 500) {
-    return Promise.resolve(jsonwebtoken_1.default.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)));
-}
 exports.token = token;
+exports.default = void 0;
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+const signOption = (issuerId, privateKeyId, duration)=>({
+        algorithm: 'ES256',
+        keyid: privateKeyId,
+        audience: 'appstoreconnect-v1',
+        expiresIn: duration,
+        issuer: issuerId
+    })
 ;
+function jwtGenCore() {}
+function tokenSync(privateKey, issuerId, privateKeyId, duration = 500) {
+    return _jsonwebtoken.default.sign(JSON.parse("{}"), privateKey, signOption(issuerId, privateKeyId, duration));
+}
+async function token(privateKey, issuerId, privateKeyId, duration = 500) {
+    return Promise.resolve(_jsonwebtoken.default.sign(JSON.parse("{}"), privateKey, signOption(issuerId, privateKeyId, duration)));
+}
 jwtGenCore.tokenSync = tokenSync;
 jwtGenCore.token = token;
 module.exports = exports = jwtGenCore;
-exports.default = jwtGenCore;
+var _default = jwtGenCore;
+exports.default = _default;
+
+
 //# sourceMappingURL=index.cjs.map
