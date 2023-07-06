ee1436ab1da8e78cba69060b573445c7b314738b
Kenji Saito <ken-yo@mbr.nifty.com>
2023-07-07 08:31:17 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -20,7 +20,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-yarn install && yarn upgrade && yarn format && yarn lint --fix && rm -rf dist && yarn build && yarn package && rm -rf node_modules && yarn install --production && git add dist node_modules -f
+pnpm install && pnpm up && pnpm format && pnpm lint --fix && rm -rf dist && pnpm build && pnpm package && rm -rf node_modules && pnpm install --production && git add dist node_modules -f
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
